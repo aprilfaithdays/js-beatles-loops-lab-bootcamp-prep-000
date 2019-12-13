fc44ecde9e8f4592cb2aafb4ee8582f3968cc720
April Escobar <aprilfaith.e@gmail.com>
2019-12-13 03:52:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments) {
+  
+  var instrumentsMusiciansPlay = [];
+  
+  for ( let i = 0; i < musicians.length; i++){
+
+    instrumentsMusiciansPlay.push( musicians[i] + " plays " + instruments[i]);
+  }
+  
+  return instrumentsMusiciansPlay;
+}
+
+
+function johnLennonFacts(facts) {
+  
+  for (let f = 0; f < facts.length; f++){
+  
+    while (f < facts.length) {
+    
+    return facts.push( facts[f] + "!!!");
+    }
+  }
+}
